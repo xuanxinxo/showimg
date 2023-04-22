625186efc78345a6ecbfe64e4fc2c3a26dce36db
xuanxinxo <117167585+xuanxinxo@users.noreply.github.com>
2023-04-22 14:00:59 +0700

    Add files via upload

---
+++ Hooks.js
@@ -0,0 +1,22 @@
+// import React, { Component } from 'react'
+
+// class Hooks extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         count: 0
+//       };
+//     }
+  
+//     render() {
+//       return (
+//         <div>
+//           <p>Bạn đã bấm {this.state.count} lần</p>
+//           <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+//             Bấm vào tôi
+//           </button>
+//         </div>
+//       );
+//     }
+//   }
+//   export default Hooks;
---
+++ Hooksfunction.js
@@ -0,0 +1,27 @@
+// import React, { Component } from 'react'
+// // import Hooks from './Hooks'
+
+// // import React, { useState } from 'react';
+
+// // import Hooksfunction from './hooks'
+
+// function Example() {
+//   // Khai báo một biến state mới, gọi nó là "count"
+//   const [count, setCount] = useState(0)
+
+// return(
+//     <div>
+//     <p>Bạn đã bấm {this.state.count} lần</p>
+//     <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+//       Bấm vào tôi
+//     </button>
+//   </div>
+// )
+
+
+ 
+
+
+
+// };
+// export default Hooksfunction;
